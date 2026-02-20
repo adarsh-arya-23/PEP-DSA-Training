89fdf30afb6aa523a47f47530785c7e73708d3aa
Adarsh Arya <adarsharya7073@gmail.com>
2026-02-20 19:19:24 +0530

    07 Quick sort spacing modified

--- Day 9/07_QuickSort.cpp
+++ Day 9/07_QuickSort.cpp
@@ -16,22 +16,18 @@ using namespace std;
 
 int main() {
     vector<int> v = {9,8,7,6,5,4,3,2,1};
-
     function<void(int,int)> quickSort = [&](int low, int high) {
         if (low < high) {
             int pivot = v[high];
             int i = low - 1;
-
             for (int j = low; j < high; j++) {
                 if (v[j] < pivot) {
                     i++;
                     swap(v[i], v[j]);
                 }
             }
-
             swap(v[i + 1], v[high]);
             int pi = i + 1;
-
             quickSort(low, pi - 1);
             quickSort(pi + 1, high);
         }
