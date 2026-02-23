dd8f582367ef8a94abe64b03e604cc0fc69a8ab9
Adarsh Arya <adarsharya7073@gmail.com>
2026-02-23 13:37:06 +0530

    01 BackTracking

--- Day 10/01_BackTracking.cpp
+++ Day 10/01_BackTracking.cpp
@@ -16,3 +16,65 @@ Leetcode  BrutForce Approach:- 37. Sudoku Solver
 
 
 */
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution{
+public:
+    bool isValid(vector<vector<char>> &board, int r, int c, char num){
+        for (int i = 0; i < 9; i++){
+            if (board[r][i] == num)
+                return false;
+            if (board[i][c] == num)
+                return false;
+            if (board[3 * (r / 3) + i / 3][3 * (c / 3) + i % 3] == num)
+                return false;
+        }
+        return true;
+    }
+    bool sol(vector<vector<char>> &board){
+        for (int r = 0; r < 9; r++){
+            for (int c = 0; c < 9; c++){
+                if (board[r][c] == '.'){
+                    for (char num = '1'; num <= '9'; num++){
+                        if (isValid(board, r, c, num)){
+                            board[r][c] = num;
+                            if (sol(board))
+                                return true;
+                            else
+                                board[r][c] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>> &board){
+        sol(board);
+    }
+};
+    int main(){
+        Solution s;
+        vector<vector<char>> board = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        s.solveSudoku(board);
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+        return 0;
+    }
