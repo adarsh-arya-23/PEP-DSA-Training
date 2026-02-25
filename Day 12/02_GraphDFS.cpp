a9e4afe88c556e4ec51e110a7c584ee1ed91529d
Adarsh Arya <adarsharya7073@gmail.com>
2026-02-25 15:25:35 +0530

    02 DFS Graph , 03 BFS Graph

--- Day 12/02_GraphDFS.cpp
+++ Day 12/02_GraphDFS.cpp
@@ -2,30 +2,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited){
     visited[node] = true;
     cout << node << " ";
-    
-    for (int neighbor : adj[node]) {
-        if (!visited[neighbor]) {
+
+    for (int neighbor : adj[node]){
+        if (!visited[neighbor]){
             dfs(neighbor, adj, visited);
         }
     }
 }
 
-int main() {
+int main(){
     int vertices = 5;
     vector<vector<int>> adj(vertices);
-    
     adj[0].push_back(1);
     adj[1].push_back(0);
     adj[0].push_back(2);
     adj[2].push_back(0);
     adj[1].push_back(3);
     adj[3].push_back(1);
-    adj[2].push_back(4);
-    adj[4].push_back(2);
-    
     vector<bool> visited(vertices, false);
     
     cout << "DFS traversal: ";
--- Day 12/03_GraphBFS.cpp
+++ Day 12/03_GraphBFS.cpp
@@ -3,36 +3,36 @@
 #include <queue>
 using namespace std;
 
-class Graph {
+class Graph{
 private:
     int vertices;
     vector<vector<int>> adjacencyList;
 
 public:
-    Graph(int v) {
+    Graph(int v){
         vertices = v;
         adjacencyList.resize(v);
     }
 
-    void addEdge(int u, int v) {
+    void addEdge(int u, int v){
         adjacencyList[u].push_back(v);
         adjacencyList[v].push_back(u);  
     }
 
-    void BFS(int start) {
+    void BFS(int start){
         vector<bool> visited(vertices, false);
         queue<int> q;
 
         q.push(start);
         visited[start] = true;
 
-        while (!q.empty()) {
+        while (!q.empty()){
             int node = q.front();
             q.pop();
             cout << node << " ";
 
-            for (int neighbor : adjacencyList[node]) {
-                if (!visited[neighbor]) {
+            for (int neighbor : adjacencyList[node]){
+                if (!visited[neighbor]){
                     visited[neighbor] = true;
                     q.push(neighbor);
                 }
@@ -42,7 +42,7 @@ public:
     }
 };
 
-int main() {
+int main(){
     Graph g(6);
     
     g.addEdge(0, 1);
@@ -52,7 +52,6 @@ int main() {
     g.addEdge(2, 3);
     g.addEdge(3, 4);
     g.addEdge(4, 5);
-
     cout << "BFS starting from vertex 0: ";
     g.BFS(0);
 
