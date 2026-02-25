9855a4f1e23c70c9494334e889893f5f089b6d25
Adarsh Arya <adarsharya7073@gmail.com>
2026-02-25 15:26:51 +0530

    01 Nqueen

--- Day 12/01_NQueen.cpp
+++ Day 12/01_NQueen.cpp
@@ -6,30 +6,30 @@ int n;
 vector<vector<int>> board;
 int solutionCount = 0;
 
-bool isSafe(int row, int col) {
-    for (int i = 0; i < row; i++) {
+bool isSafe(int row, int col){
+    for (int i = 0; i < row; i++){
         if (board[i][col] == 1) return false;
     }
     
-    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--){
         if (board[i][j] == 1) return false;
     }
     
-    for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+    for (int i = row, j = col; i >= 0 && j < n; i--, j++){
         if (board[i][j] == 1) return false;
     }
     
     return true;
 }
 
-void solve(int row) {
-    if (row == n) {
+void solve(int row){
+    if (row == n){
         solutionCount++;
         return;
     }
     
-    for (int col = 0; col < n; col++) {
-        if (isSafe(row, col)) {
+    for (int col = 0; col < n; col++){
+        if (isSafe(row, col)){
             board[row][col] = 1;
             solve(row + 1);
             board[row][col] = 0; 
@@ -37,9 +37,9 @@ void solve(int row) {
     }
 }
 
-void printBoard() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+void printBoard(){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
             cout << (board[i][j] ? "Q " : ". ");
         }
         cout << "\n";
@@ -47,12 +47,10 @@ void printBoard() {
     cout << "\n";
 }
 
-int main() {
+int main(){
     cin >> n;
     board.assign(n, vector<int>(n, 0));
-    
     solve(0);
     cout << "Total solutions: " << solutionCount << "\n";
-    
     return 0;
 }
